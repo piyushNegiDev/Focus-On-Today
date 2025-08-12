01d19dab8eb49f8cacc3bcf6f0123292140a7f5b
piyushNegiDev <piyushnegi381@gmail.com>
2025-08-12 20:32:17 +0530

    Wrap code insides functions

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-let html = "";
-for (let index = 0; index < 3; index++) {
-  html += `
-    <div class="input-container">
-    <div class="circle"></div>
-    <div class="input">
-    <input type="text" id=${index} placeholder="Add new goal" />
-    </div>
-    </div>`;
+function goalsHtml() {
+  let html = "";
+  for (let index = 0; index < 3; index++) {
+    html += `
+        <div class="input-container">
+        <div class="circle"></div>
+        <div class="input" id=${index}>
+        <input type="text" placeholder="Add new goal" />
+        </div>
+        </div>`;
+  }
+  document.querySelector(".goals").innerHTML = html;
 }
-document.querySelector(".goals").innerHTML = html;
 
-let inputs = document.getElementsByTagName("input");
-inputs = Array.from(inputs);
-inputs.forEach((input) => {
-  input.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-      input.parentElement.innerHTML = input.value;
-    }
+function inputsListener() {
+  let inputs = document.getElementsByTagName("input");
+  inputs = Array.from(inputs);
+  inputs.forEach((input) => {
+    input.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        input.parentElement.innerHTML = input.value;
+      }
+    });
   });
-});
+}
 
-let circles = document.querySelectorAll(".circle");
-circles.forEach((circle) => {
-  circle.addEventListener("click", () => {
-    circle.innerHTML = `<img src="green tick.png" width="19" height="19" alt="" />`;
-    circle.style.border = "none";
+function tick() {
+  let circles = document.querySelectorAll(".circle");
+  circles.forEach((circle) => {
+    circle.addEventListener("click", () => {
+      circle.innerHTML = `<img src="green tick.png" width="19" height="19" alt="" />`;
+      circle.style.border = "none";
+    });
   });
-});
+}
+
+goalsHtml();
+inputsListener();
+tick();
